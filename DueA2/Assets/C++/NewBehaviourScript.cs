27635144104c25b823936a83311e479fe5811bcc
PaiDaXingIII <162035030+PaiDaXingIII@users.noreply.github.com>
2025-03-28 21:06:45 +0800

    Fourth submission
    Added buttons and page jump content

---
+++ DueA2/Assets/C++/NewBehaviourScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManipulator : MonoBehaviour
+{
+    public GameObject tube;
+    [SerializeField] float value;
+    [SerializeField] Vector3 sizeChangeValue;
+    private Vector3 startPosition;
+
+    public void Start()
+    {
+        startPosition = new Vector3(tube.transform.position.x, tube.transform.position.y, tube.transform.position.z);
+    }
+
+    public void MoveRight()
+    {
+        tube.transform.position = new Vector3(tube.transform.position.x + value,
+            tube.transform.position.y, tube.transform.position.z);
+    }
+
+    public void RotateObject()
+    {
+        tube.transform.Rotate(0, 20f, 0);
+    }
+
+    public void GrowObject()
+    {
+        tube.transform.localScale = tube.transform.localScale + sizeChangeValue;
+    }
+
+    public void ResetObject()
+    {
+        tube.transform.position = startPosition;
+        tube.transform.rotation = Quaternion.Euler(new Vector3(0, 0, -20));
+        tube.transform.localScale = Vector3.one;
+    }
+}
